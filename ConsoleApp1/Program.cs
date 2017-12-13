c38b57a3b3601bd77d3788ac7ed4d29d163e7af7
lorraine <randomlycoding@gmail.com>
2017-12-13 15:20:51 +0300

    upload files

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle = new Triangle();
+            Random random = new Random();
+            int k1, k2; //Катеты
+            double g;   //Гипотенуза
+            for (int i = 0; i < 10; i++) //Заполняем, считаем и выводим в консоль
+            {
+                k1 = random.Next(1, 100);
+                k2 = random.Next(1, 100);
+                g = Math.Sqrt(k1 * k1 + k2 * k2);
+                triangle.SetSides(k1, k2, g);
+                Console.WriteLine("Sides:  " + triangle.a + ", " + triangle.b + ", " + triangle.c);
+                Console.WriteLine("P / 2:  " + triangle.p);
+                Console.WriteLine("Square: " + triangle.Square());
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/Triangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Triangle
+    {
+        public int a;
+        public int b;
+        public double c;
+        public double p;
+
+        public Triangle()
+        {
+            a = 0;
+            b = 0;
+            c = 0;
+            p = 0;
+        }
+        
+        public Triangle(int SideA, int SideB, double SideC)
+        {
+            a = SideA;
+            b = SideB;
+            c = SideC;
+            p = (a + b + c) / 2;
+        }
+
+        public void SetSides(int SideA, int SideB, double SideC)
+        {
+            a = SideA;
+            b = SideB;
+            c = SideC;
+            p = (a + b + c) / 2;
+        }
+
+        // В условии сказано, что треугольник прямоугольный, поэтому
+        // было бы логичнее считать его как половину произведения катетов
+        // но ладно, по трём сторонам так по трём сторонам
+        public double Square()
+        {
+            double sq;
+            sq = Math.Sqrt(p * (p - a) * (p - b) * (p - c)); 
+            return sq;
+        }
+    }
+}
